a297234eb10f3215aaa050dbe23a1cbf43de5541
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-12-19 20:26:15 -0500

    rendered fake user data, added styling

--- src/components/UserProfile/edituserprofile.js
+++ src/components/UserProfile/edituserprofile.js
@@ -1,9 +1,20 @@
+const fakeUser = {
+    username: 'idkw',
+    password: '1234',
+    phoneNumber: '123-555-6666'
+}
+
 export default function CreatePlant() {
 
     return (
         <div>
             <h1>USER PROFILE</h1>
             <p>Show all User's info and allow updating of password and phone number</p>
+            <div className='user-info'>
+                <p>Username: {fakeUser.username}</p>
+                <p>Password: {fakeUser.password}</p>
+                <p>Phone Number: {fakeUser.phoneNumber}</p>
+            </div>
         </div>
     )
 }
