49304ab5993f0cbece7927a48d0af6b4dbb6e3e8
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-12-22 03:39:57 -0500

    export default for axiosWithAuth

--- src/components/utils/axiosWithAuth.js
+++ src/components/utils/axiosWithAuth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const axiosWithAuth = () => {
+const axiosWithAuth = () => {
     const token = localStorage.getItem('token')
 
     return axios.create({
@@ -11,3 +11,5 @@ export const axiosWithAuth = () => {
 
     })
 }
+
+export default axiosWithAuth;
