44496fd9c7ca14160b097ac6aa79c59791eb7853
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-12-21 20:38:47 -0500

    commented out last two schemas

--- src/validation/formSchemas.js
+++ src/validation/formSchemas.js
@@ -40,26 +40,26 @@ export const editUserSchema = Yup.object().shape({
         .min(10, 'Must have 10 numbers')
 })
 
-export const addPlantSchema = Yup.object().shape({
-    nickname: Yup
-        .string()
-        .required("nickname is required"),
-    species: Yup
-        .string()
-        .required("species is required"),
-    h2OFrequency: Yup
-        .string()
-        .required("Enter a number")
-})
+// export const addPlantSchema = Yup.object().shape({
+//     nickname: Yup
+//         .string()
+//         .required("nickname is required"),
+//     species: Yup
+//         .string()
+//         .required("species is required"),
+//     h2OFrequency: Yup
+//         .string()
+//         .required("Enter a number")
+// })
 
-export const editPlantSchema = Yup.object().shape({
-    nickname: Yup
-        .string()
-        .required("nickname is required"),
-    species: Yup
-        .string()
-        .required("species is required"),
-    h2OFrequency: Yup
-        .string()
-        .required("Enter a number")
-})
+// export const editPlantSchema = Yup.object().shape({
+//     nickname: Yup
+//         .string()
+//         .required("nickname is required"),
+//     species: Yup
+//         .string()
+//         .required("species is required"),
+//     h2OFrequency: Yup
+//         .string()
+//         .required("Enter a number")
+// })
